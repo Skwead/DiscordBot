76852b8c63edba70c7639724c65b6269da3387c7
Paulo <pauloeduardo2709@gmail.com>
2019-01-05 15:12:57 -0200

    ✨ 🎨 Add tries count to captcha

--- src/main/java/br/com/finalelite/bots/supporter/utils/Captcha.java
+++ src/main/java/br/com/finalelite/bots/supporter/utils/Captcha.java
@@ -3,21 +3,34 @@ package br.com.finalelite.bots.supporter.utils;
 import com.octo.captcha.service.CaptchaServiceException;
 import com.octo.captcha.service.image.DefaultManageableImageCaptchaService;
 import com.octo.captcha.service.image.ImageCaptchaService;
+import lombok.val;
 
 import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class Captcha {
 
-    private ImageCaptchaService service = new DefaultManageableImageCaptchaService();
+    private final ImageCaptchaService service = new DefaultManageableImageCaptchaService();
+    private final Map<String, Integer> ids = new HashMap<>();
 
     public byte[] createNewCaptcha(String id) {
-        ByteArrayOutputStream jpegOutputStream = new ByteArrayOutputStream();
+        ids.put(id, 1);
+        return getImage(id);
+    }
+
+    public byte[] createAnotherCaptcha(String id) {
+        ids.put(id, ids.get(id) + 1);
+        return getImage(id);
+    }
+
+    private byte[] getImage(String id) {
+        val jpegOutputStream = new ByteArrayOutputStream();
         try {
-            BufferedImage challenge =
+            val challenge =
                     service.getImageChallengeForID(id, Locale.ENGLISH);
 
             ImageIO.write(challenge, "jpg", jpegOutputStream);
@@ -26,8 +39,11 @@ public class Captcha {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return jpegOutputStream.toByteArray();
+    }
+
+    public int getTries(String id) {
+        return ids.getOrDefault(id, 0);
     }
 
     public boolean check(String id, String text) {
