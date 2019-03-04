0fa65e6e0d0f00d9645ba0e36e8d26f8dfa409ff
Paulo <pauloeduardo2709@gmail.com>
2019-03-04 19:47:20 -0300

    🎨 ✨ Add the Emote in the ticket type

--- src/main/java/br/com/finalelite/bots/supporter/ticket/TicketStatus.java
+++ src/main/java/br/com/finalelite/bots/supporter/ticket/TicketStatus.java
@@ -1,12 +1,28 @@
 package br.com.finalelite.bots.supporter.ticket;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+
+@RequiredArgsConstructor
+@Getter
 public enum TicketStatus {
-    CLOSED,
-    OPENED,
-    SPAM;
+    CLOSED("\uD83D\uDC97"),
+    OPENED("\uD83D\uDC9A"),
+    SPAM("\uD83D\uDDA4");
+
+    private final String emoji;
 
     public static TicketStatus getFromOrdinalId(byte id) {
         return TicketStatus.values()[id];
+    }
+
+    public static TicketStatus getByEmoji(String emoji) {
+        return Arrays.stream(TicketStatus.values())
+                .filter(status -> status.getEmoji().equals(emoji))
+                .findFirst()
+                .orElse(null);
     }
 
 }
